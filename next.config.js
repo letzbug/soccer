a620c7115199d5bf192ca2c9396d83f34509d95e
letzbug <129280428+letzbug@users.noreply.github.com>
2026-06-04 19:21:05 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({ dest: 'public', register: true, skipWaiting: true, disable: process.env.NODE_ENV === 'development' })
-module.exports = withPWA({ reactStrictMode: true, images: { remotePatterns: [{ protocol: 'https', hostname: '**' }] } })
+const nextConfig = {
+  turbopack: {}
+};
+
+module.exports = nextConfig;
